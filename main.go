d6044b58d4d681c93c1e03ca86a3f4ee80777bcf
Rahul <rahul@g3ck0.(none)>
2013-12-02 17:19:59 -0500

    added gobject store/load functions

--- main.go
+++ main.go
@@ -21,6 +21,29 @@ type depinfo struct {
 }
 
 
+func store(data interface{} , filename string) {
+  m := new(bytes.Buffer) 
+  enc := gob.NewEncoder(m)
+
+  err := enc.Encode(data)
+  if err != nil { panic(err) }
+
+  err = ioutil.WriteFile(filename, m.Bytes(), 0600) 
+  if err != nil { panic(err) }
+}
+
+
+func load(e interface{} , filename string) {
+    n,err := ioutil.ReadFile(filename)
+    if err != nil { panic(err) } 
+
+    p := bytes.NewBuffer(n) 
+    dec := gob.NewDecoder(p)
+
+    err = dec.Decode(e)
+    if err != nil { panic(err) }
+}
+
 // Retrieves the hash_values stored as a gobject hash_store
 func hash_ret (gobject string ) map[string]string{
   n,err := ioutil.ReadFile(gobject)

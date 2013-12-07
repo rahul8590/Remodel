53eb71378bd4e7190b7ea9486639670f1e8bd4fd
Rahul <rahul@g3ck0.(none)>
2013-12-06 19:45:47 -0500

    added check() for file/dir existence

--- main.go
+++ main.go
@@ -308,6 +308,14 @@ func exe_cmd(cmd string, wg *sync.WaitGroup) {
 
 
 
+func check(path string) (bool) {
+    _, err := os.Stat(path)
+    if err == nil { return true }
+    if os.IsNotExist(err) { return false }
+    return false
+}
+
+
 
 func main(){
 
